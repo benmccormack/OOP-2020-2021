13bb0b3ed32ae8fba0c8ede03248b56146d5ecbd
benmccormack <c19459424@mytudublin.ie>
2021-02-03 16:08:58 +0000

    Monday Class Week 2

--- java/src/ie/tudublin/HelloProcessing2.java
+++ java/src/ie/tudublin/HelloProcessing2.java
@@ -4,20 +4,31 @@ import processing.core.PApplet;
 
 public class HelloProcessing2 extends PApplet
 {
-
+	//This is the first set of code to be executed. Were stating the size of whre we can draw
 	public void settings()
 	{
 		size(500, 500);
 	}
 
+	//This gets called once . Colors
 	public void setup() {
 		
 	}
 	
+	//This method is called 60 times a second. Already in a method from its super class
 	public void draw()
 	{	
-		background(0);
-		stroke(255);
-		line(10, 10, 100, 100);
+		background(255,0,0); //sets background color takes 1 or 3 or 4 parameters
+		stroke(255); //sets the pen colour
+		fill(255, 247, 0);
+		ellipse(250,250,400,400); //drawing a circle cx,cy,w,h
+		fill(20, 240, 247); //the fill colour
+		triangle(250,30,30,400,470,400); //x1,y1,x2,y2, x3 y3
+		fill(255, 255, 255);
+		stroke(255, 255, 255);
+		ellipse(250,250,250,100); //drawing a circle cx,cy,w,h
+		fill(0, 0, 0);
+		stroke(0, 0, 0);
+		ellipse(250,250,50,50); //drawing a circle cx,cy,w,h
 	}
 }
