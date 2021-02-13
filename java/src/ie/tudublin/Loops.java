e215cf35d1f49818e8af6c9a1edb2990352e4aa7
benmccormack <c19459424@mytudublin.ie>
2021-02-13 15:47:11 +0000

    Monday Class Wk 3

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -6,6 +6,7 @@ public class Loops extends PApplet {
 
     public void settings() {
         size(500, 500);
+        //These are two useful variables we are going to use in our program
         cx = width / 2;
         cy = height / 2;        
     }
@@ -18,21 +19,75 @@ public class Loops extends PApplet {
     public void keyPressed() {
         // the value of mode will be the number of the 
         // number key pressed
+        // assigning key pressed to the mode
         if (keyCode >= '0' && keyCode <= '9')
             mode = keyCode - '0';
     }
 
     public void setup() {
+        // setting the color mode to be HSB - Hue, Saturation, Brightness
         colorMode(HSB);
     }
 
     public void draw() {
         background(0);
+        noStroke();
         switch (mode)
         {
             case 0:
-                ellipse(cx, cy, 100, 100);
+            //build in variable mouseX and mouseY are from the pApplet super class
+                fill(50,255,255);
+                if(mouseX < cx)
+                {
+                    rect(0, 0, cx, height);
+                }
+                else
+                {
+                    rect(cx, 0, cx, height);
+                }
                 break;
+            
+            case 1:
+                fill(50,255,255);
+                if(mouseX < cx && mouseY < cy)
+                {
+                    rect(0, 0, cx, cy);
+                }
+                else if(mouseX > cx && mouseY < cy)
+                {
+                    rect(cx, 0, cx, cy);
+                }
+
+                if(mouseY > cy && mouseX < cx)
+                {
+                    rect(0, cy, cx, cy);
+                }
+                else if(mouseX > cx && mouseY > cy)
+                {
+                    rect(cx, cy, cx, cy);
+                }
+                break;
+            
+            case 2:
+                int numRects = 10;
+                float w = width/(float) numRects;
+                float cgap = 255 / (float) numRects;
+                for (int i = 0; i < numRects; i++)
+                {
+                    fill(i * cgap, 255, 255);
+                    rect(i * w, 0, w, height);
+                }
+            
+            case 3:
+                int numCircles = 10;
+                float w = width/(float) numCircles;
+                float cgap = 255 / (float) numCircles;
+                for (int i = 0; i < numRects; i++)
+                {
+                    fill(i * cgap, 255, 255);
+                    ellipse(w / 2 + (i * w), cy ,w ,w);
+                }
+
         }
     }
 }
