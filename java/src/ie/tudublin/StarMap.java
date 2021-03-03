a07b69a3410421271b837fad91be09f66e1e34d5
benmccormack <c19459424@mytudublin.ie>
2021-03-03 16:23:12 +0000

    Monday Class 6

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -1,8 +1,34 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class StarMap extends PApplet {
+
+    //use array list if you dont know how many elements are in the array
+    ArrayList<star> stars = new ArrayList<star>();
+    
+    void printStars()
+    {
+        for(star s : stars){
+            println(s);
+        }
+    }
+
+    void loadStars()
+    {
+        Table table = loadTable("HabHYG15ly.csv", "header");
+        //enhanced for loop specifically for iterating over collections
+        for(TableRow row:table.rows())
+        {
+            star s = new star(row);
+            stars.add(s);
+        }
+
+    }
+
     public void settings() {
         size(500, 500);
     }
@@ -14,6 +40,8 @@ public class StarMap extends PApplet {
 
     public void setup() {
         colorMode(RGB);
+        loadStars();
+        printStars();
     }
 
     public void draw() {
---
+++ java/src/ie/tudublin/star.java
@@ -0,0 +1,103 @@
+package ie.tudublin;
+
+import processing.data.TableRow;
+
+public class star {
+    private boolean hab;
+    private String displayName;
+    private float distance;
+    private float xG, yG, zG;
+    private float absMag;
+
+    public String toString(){
+        return hab + "\t" + displayName +"\t" + distance + "\t" + xG + "\t" + yG + "\t" + zG;
+    }
+
+    //constructors do not have a return method
+    public star()
+    {
+
+    }
+
+    public star(TableRow row)
+    {
+        // constructor chaining - when you have lots of different constructors you can call one from another
+        this(
+            row.getInt("Hab?") == 1 ? true : false,
+            row.getString("Display Name"),
+            row.getFloat("Distance"),
+            row.getFloat("Xg"),
+            row.getFloat("Yg"),
+            row.getFloat("Zg"),
+            row.getFloat("AbsMag")
+            );
+
+    }
+
+    public star(boolean hab, String displayName, float distance, float xG, float yG, float zG, float absMag){
+        this.hab = hab;
+        this.displayName = displayName;
+        this.distance = distance;
+        this.xG = xG;
+        this.yG = yG;
+        this.zG = zG;
+        this.absMag = absMag;
+    }
+
+    //toString method is a method that gets called automatically whenever we pass to something that expects a string
+    public boolean isHab() {
+        return hab;
+    }
+
+    public void setHab(boolean hab) {
+        this.hab = hab;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public float getDistance() {
+        return distance;
+    }
+
+    public void setDistance(float distance) {
+        this.distance = distance;
+    }
+
+    public float getxG() {
+        return xG;
+    }
+
+    public void setxG(float xG) {
+        this.xG = xG;
+    }
+
+    public float getyG() {
+        return yG;
+    }
+
+    public void setyG(float yG) {
+        this.yG = yG;
+    }
+
+    public float getzG() {
+        return zG;
+    }
+
+    public void setzG(float zG) {
+        this.zG = zG;
+    }
+
+    public float getAbsMag() {
+        return absMag;
+    }
+
+    public void setAbsMag(float absMag) {
+        this.absMag = absMag;
+    }
+}
