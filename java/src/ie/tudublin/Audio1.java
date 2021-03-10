7c604e12bb7b20960f6bd661fc431b5a46f7440e
benmccormack <c19459424@mytudublin.ie>
2021-03-10 16:23:45 +0000

    Monday Class 7

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -27,10 +27,10 @@ public class Audio1 extends PApplet {
     {
         minim = new Minim(this);
         ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
-        //ap = minim.loadFile("scale.wav", width);        
-        // ap.play();
-        ab = ai.mix; // Connect the buffer to mic
-        //ab = ap.mix; // Connect the buffer to the mp3 file
+        ap = minim.loadFile("scale.wav", width);        
+        ap.play();
+        //ab = ai.mix; // Connect the buffer to mic
+        ab = ap.mix; // Connect the buffer to the mp3 file
         colorMode(HSB);
         lerpedBuffer = new float[width];
 
@@ -48,24 +48,24 @@ public class Audio1 extends PApplet {
         float sum = 0;
         for(int i = 0 ; i < ab.size(); i ++)
         {
-            float c = map(i, 0, ab.size(), 0, 255);
-            stroke(c, 255, 255);
+            //float c = map(i, 0, ab.size(), 0, 255);
+            stroke(255, 255, 255);
             lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.1f);
 
-            line(i, halfHeight - lerpedBuffer[i] * halfHeight * 4, halfHeight + lerpedBuffer[i] * halfHeight * 4, i);
+            line(i, halfHeight - lerpedBuffer[i] * halfHeight * 4,  i, halfHeight + lerpedBuffer[i] * halfHeight * 4);
             //println(ab.get(i));
             sum += abs(ab.get(i));
         }
         average = sum / (float) ab.size();
         // Calculate the AVERAGE amplitude
-        lerpedAverage = lerp(lerpedAverage, average, 0.1f);
-        ellipse(width / 4, 100, average * 500, average * 500);
-        ellipse(width / 2, 100, 50 + (lerpedAverage * 500), 50 + (lerpedAverage * 500));
+        //lerpedAverage = lerp(lerpedAverage, average, 0.1f);
+        //ellipse(width / 4, 100, average * 500, average * 500);
+        //ellipse(width / 2, 100, 50 + (lerpedAverage * 500), 50 + (lerpedAverage * 500));
 
         
-        ellipse(200, y, 30, 30);
-        ellipse(300, lerpedY, 30, 30);
-        y += random(-10, 10);
-        lerpedY = lerp(lerpedY, y, 0.1f);        
+        //ellipse(200, y, 30, 30);
+        //ellipse(300, lerpedY, 30, 30);
+        //y += random(-10, 10);
+        //lerpedY = lerp(lerpedY, y, 0.1f);        
     }   
 }
